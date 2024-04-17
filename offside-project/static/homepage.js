e6480661573696f42bc9c30588469b4c39539769
Maximo Jalife <maximojalife@Maximos-MBP-2.lan>
2024-04-17 11:12:52 -0400

    added redirection of start quiz button

--- offside-project/static/homepage.js
+++ offside-project/static/homepage.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $("#startlearning").click(function(){
+        console.log("Start learning clicked!");
         window.location.href = "/lesson/1";
+    });
+
+    $("#startquiz").click(function(){
+        console.log("Start quiz clicked!");
+        window.location.href = "/quizStart";
     });
 });
