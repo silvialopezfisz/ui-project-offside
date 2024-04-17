d2289e9ca62a3d8f53e277b2145e73ac783f543f
Harold-coder <hjc2154@columbia.edu>
2024-04-16 22:55:54 -0400

    Added some info

--- offside-project/server.py
+++ offside-project/server.py
@@ -3,37 +3,58 @@ from flask import render_template
 from flask import Response, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
+
 lessons = {
     "1": {
         "number": "1",
-        "media": "youtube.com"
+        "media": "https://i.ibb.co/TKhYwTP/Screenshot-2024-04-16-at-22-54-52.png",
+        "title": "The Definition",
+        "description": "A player is in an offside position when an attacking player is nearer to the opponent’s goal line than both the ball and the second to last opponent."
         },
     "2": {
         "number": "2",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "The Basics",
+        "description": "Only the team with possession of the ball can be offside. Offside can only occur on the attacking side of the field."
         },
     "3": {
         "number": "3",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "",
+        "description": ""
         },
     "4": {
         "number": "4",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "",
+        "description": ""
         },
     "5": {
         "number": "5",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "",
+        "description": ""
         },
     "6": {
         "number": "6",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "",
+        "description": ""
         },
     "7": {
         "number": "7",
-        "media": "youtube.com"
+        "media": "youtube.com",
+        "title": "",
+        "description": ""
         }
     }
 
+learning_questions = {
+   "1": {
+      "number": "1",
+      "media": "https://vimeo.com/935697965?share=copy"
+   }
+}
 questions = {
     "1": {
         "Qnumber": "1",
